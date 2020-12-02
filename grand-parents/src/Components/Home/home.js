6a9d56723bb818a41537acc6f8ffd3841c352a8a
julian <julian.loaiza@uao.edu.co>
2020-12-02 03:15:04 +0100

    home final

--- grand-parents/src/Components/Home/home.js
+++ grand-parents/src/Components/Home/home.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import imghome from '../../Assets/icons/img_inicio.png';
+import './home.css';
+import imghome from '../../Assets/img/img_inicio.png';
 
 function Home(){
     return(
-        <div>
-            
-            <img src={imghome}/>
+        <div className="o-container-inicio">
+            <div className="o-container-body-home">
+                <div className="o-container-left">
+                    <div className="o-detail-left">
+                        <h1>Actividades <br/> lúdicas</h1>
+                    <p className="o-paragraph">
+                        Nuestra meta es ayudar <br/>  a las personas mayores,<br/>ofreciéndoles distintas <br/> actividades para  <br/> su tiempo en casa.
+                    </p>
+
+                   <div><button className="o-btn-login">Iniciar Sesión</button> </div>
+
+                    </div> 
+                </div>
+        
+            <div className="o-container-right">
+                <div><img className="o-img-right" src={imghome}/></div>
+            </div>
+            </div>
+            <div className="o-container-texto">
+                
+                <br/><br/>Nuestra empresa se caracteriza por brindar bienestar emocional y lúdico a las personas mayores,<br/> así como un espacio en el que se pueden divertir y desconectar de la realidad.
+                
+            </div>
         </div>
+        
     );
 }
 
